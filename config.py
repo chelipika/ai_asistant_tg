e3b9391fb7c2631fafbf4f10194735fb98ad7595
chelipika <103322024+chelipika@users.noreply.github.com>
2025-03-12 22:09:28 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
 TOKEN = ""
 GEMINI_API_KEY= ""
 CHANNEL_ID = -100  # замените на ID вашего канала оставьте -100 или же username "@superkastrulya"\ replace with your channels id after -100 or type your username "@eyemapness" e.g. -100545646545454541213545464
-CHANNEL_LINK = ""  # ссылка на канал link for cahnnel e.g. https://t.me/...........
